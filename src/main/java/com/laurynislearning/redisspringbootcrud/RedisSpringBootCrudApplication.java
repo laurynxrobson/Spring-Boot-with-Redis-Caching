fcdf110a2248ca2094cc4a11e6b95f2ec41f44d8
ct303873 <laurynrobson@capitecbank.co.za>
2025-03-11 13:42:00 +0200

    Created Project

---
+++ src/main/java/com/laurynislearning/redisspringbootcrud/RedisSpringBootCrudApplication.java
@@ -0,0 +1,13 @@
+package com.laurynislearning.redisspringbootcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisSpringBootCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisSpringBootCrudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/laurynislearning/redisspringbootcrud/RedisSpringBootCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laurynislearning.redisspringbootcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisSpringBootCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
