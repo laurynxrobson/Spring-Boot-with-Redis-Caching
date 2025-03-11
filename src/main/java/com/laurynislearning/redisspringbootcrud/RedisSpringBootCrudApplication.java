0f1cabdb0802e1af047306e40192ad1863ca96ed
ct303873 <laurynrobson@capitecbank.co.za>
2025-03-11 14:18:27 +0200

    Enabled caching in main class

--- src/main/java/com/laurynislearning/redisspringbootcrud/RedisSpringBootCrudApplication.java
+++ src/main/java/com/laurynislearning/redisspringbootcrud/RedisSpringBootCrudApplication.java
@@ -2,8 +2,10 @@ package com.laurynislearning.redisspringbootcrud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class RedisSpringBootCrudApplication {
 
     public static void main(String[] args) {
