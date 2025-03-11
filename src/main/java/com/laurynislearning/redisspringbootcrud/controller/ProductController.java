fcf595fe5d0e588e423fed1327d35580eba1a173
ct303873 <laurynrobson@capitecbank.co.za>
2025-03-11 14:13:40 +0200

    Added RESTful endpoints for CRUD operations

--- src/main/java/com/laurynislearning/redisspringbootcrud/controller/ProductController.java
+++ src/main/java/com/laurynislearning/redisspringbootcrud/controller/ProductController.java
@@ -1,4 +1,56 @@
 package com.laurynislearning.redisspringbootcrud.controller;
 
+import com.laurynislearning.redisspringbootcrud.model.Product;
+import com.laurynislearning.redisspringbootcrud.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("api/products")
 public class ProductController {
+    @Autowired
+    ProductService productService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProduct(@PathVariable Long id) {
+        Optional<Product> product = productService.getProductById(id);
+
+        return product.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+    @PostMapping
+    public Product createProduct(@RequestBody Product product) {
+        return productService.saveProduct(product);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> updateProduct(@PathVariable Long id, @RequestBody Product product) {
+        Optional<Product> existingProduct = productService.getProductById(id);
+        if (existingProduct.isPresent()) {
+            product.setId(id);
+            return ResponseEntity.ok(productService.saveProduct(product));
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
+        Optional<Product> product = productService.getProductById(id);
+        if (product.isPresent()) {
+            productService.deleteProduct(id);
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
